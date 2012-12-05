a329d407e6b7ead8021651c532f83dfc1b36d63b
Leif Grele <lgrele@gmail.com>
2012-12-05 17:14:56 -0500

    Removed some bad code
    Removed impossible entity creation and deletion code.

--- src/EntityManager.hpp
+++ src/EntityManager.hpp
@@ -29,29 +29,9 @@ template<class indexType = uint8_t,class... components>
 class EntityManager{
 public:
 	entityID createEntity(){
-		entity<sizeof...(components),indexType> e(nextID);
-		++nextID;
-		if(discardedEntities.empty() == false){
-			e.id = discardedEntities.back();
-			discardedEntities.pop_back();
-		}
-		else if (e.id > entities.size()){
-			entities.resize(e.id + 1);
-		}
-		return e.id;
 	}
 
 	void deleteEntity(const entityID id){
-		//flag component vectors for cleanup
-		dirtyflags |= entities[id].mask;
-		dirtyTuples.add(entities[id].indicies);
-		if(id == nextID - 1){
-			entities.pop_back();
-			--nextID;
-		}
-		else{
-			entities.rem
-		}
 	}
 private:
 	entityID nextID;
@@ -59,9 +39,6 @@ private:
 	std::vector<entity<sizeof...(components),indexType>> entities;
 	//tuple of vectors of components
 	vectorTuple<components...> componentVectors;
-	std::bitset<sizeof...(components)> dirtyFlags;
-	std::set<std::pair<std::bitset<sizeof...(components),std::vector<int>>> dirtyTuples;
-	std::vector<entityID> discardedEntities;
 };
 }
 #endif
--- src/SystemManager.cpp
+++ src/SystemManager.cpp
@@ -1,5 +1,6 @@
 #include "SystemManager.hpp"
 #include <algorithm>
+#include "System"
 using namespace skald;
 
 SystemManager::SystemManager():systems(0):lockSystems(false){}
@@ -7,19 +8,19 @@ SystemManager::~SystemManager(){}
 
 void SystemManager::add(const System& s){
 	auto i = std::upper_bound(systems.begin(),systems.end(),s,
-		[](const System& a,const System& b){return a.getPriority() < b.getPriority()});
+		[](const System& a,const System& b){return a->getPriority() < b->getPriority()});
 	systems.insert(i,s);
 }
 
 void SystemManager::remove(const System& s){
 	auto i = std::lower_bound(systems.begin(),systems.end(),s,
-		[](const System& a,const System& b){return a.getPriority() < b.getPriority()});
+		[](const System& a,const System& b){return a->getPriority() < b->getPriority()});
 	systems.erase(i);
 }
 
 void SystemManager::update(const double t){
 	lockSystems = true;
 	for(auto s : systems)
-		s.update(t);
+		s->update(t);
 	lockSystems = false;
 }
