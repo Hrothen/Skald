50d68a4a338ce93388885f6b4aa13baacff3cf9b
Leif Grele <lgrele@gmail.com>
2013-01-08 14:38:28 -0500

    Removed id field req from component concept
    Also removed useless Components.hpp header.

--- src/Component.hpp
+++ src/Component.hpp
@@ -14,10 +14,6 @@ public:
 		//require POD
 		static_assert(std::is_pod<C>::value == true,
 			"Compilation error: component classes should be Plain Old Data");
-		auto i = _c.id;						//check id field exists
-		std::bitset<sizeof(i) * 8> b(i);	//check convertible to bitset
-		b |= i;								//check or comparable to bitset
-		b &= i;								//check and comparable to bitset
 	}
 
 private:
